5cc52c7006812e2589205c866863065f977641a4
Cleodênia <devecleodenia@gmail.com>
2026-04-12 16:35:37 -0300

    corrigindo admin

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)  # 👈 TEM QUE VIR PRIMEIRO
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cardapio.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+
 class Produto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
     preco = db.Column(db.Float)
-    imagem = db.Column(db.String(300))
-    
+    imagem = db.Column(db.String(200))
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        preco = float(request.form['preco'])
+        imagem = request.form['imagem']
+
+        novo = Produto(nome=nome, preco=preco, imagem=imagem)
+        db.session.add(novo)
+        db.session.commit()
+
+        return redirect('/admin')
+
+    produtos = Produto.query.all()
+    return render_template('admin.html', produtos=produtos)
