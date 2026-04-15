c5ca252bbf7658b5dfd630e56a4bf3a74869d8ac
Cleodênia <devecleodenia@gmail.com>
2026-04-14 22:18:33 -0300

    corrigindo app e index

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)  # 👈 TEM que vir antes de tudo
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cardapio.db'
+db = SQLAlchemy(app)
+@app.route('/')
+def index():
+    produtos = Produto.query.all()
+    return render_template('index.html', produtos=produtos)
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method == 'POST':
