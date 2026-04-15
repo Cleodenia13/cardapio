4d35430e1865591a51b91b388046beac0e808a82
Cleodênia <devecleodenia@gmail.com>
2026-04-14 22:02:16 -0300

    corrigindo erro 500

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cardapio.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Produto(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    preco = db.Column(db.Float)
-    imagem = db.Column(db.String(200))
-
-@app.route('/')
-def index():
-    produtos = Produto.query.all()
-    return render_template('index.html', produtos=produtos)
-
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method == 'POST':
@@ -34,6 +13,3 @@ def admin():
 
     produtos = Produto.query.all()
     return render_template('admin.html', produtos=produtos)
-
-if __name__ == '__main__':
-    app.run(debug=True)
